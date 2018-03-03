810787e76f57180abe896bea5f806d048c3218f9
Michael Richters <gedankenexperimenter@gmail.com>
2018-03-02 23:07:02 -0600

    Added KeyswitchState data type

---
+++ src/kaleidoscope/KeyswitchState.h
@@ -0,0 +1,54 @@
+// -*- c++ -*-
+
+#pragma once
+
+#include <Arduino.h>
+
+
+namespace kaleidoscope {
+
+union KeyswitchState {
+
+ public:
+
+  byte raw;
+  struct {
+    byte current : 1, previous : 1, reserved : 6;
+  };
+
+  // Constructor sets all reserved bits to zero.
+  explicit constexpr
+  KeyswitchState(byte state = 0)
+    : raw(state & 0x03) {}
+
+  constexpr
+  KeyswitchState(byte curr, byte prev)
+    : current(curr), previous(prev), reserved(0) {}
+
+  // These comparisons all assume that the reserved bits are all zero.
+  bool isInactive() const {
+    return raw == 0; // 0b00 (previous = 0; current = 0;)
+  }
+
+  bool isPressed() const {
+    return raw == 1; // 0b01 (previous = 0; current = 1;)
+  }
+
+  bool isReleased() const {
+    return raw == 2; // 0b10 (previous = 1; current = 0;)
+  }
+
+  bool isHeld() const {
+    return raw == 3; // 0b11 (previous = 1; current = 1;)
+  }
+
+  // This operator is probably unnecessary; the above functions should get us everything we need
+  bool operator!() const {
+    // return (! current && ! previous);
+    // return !(raw & B11);
+    return (! raw);
+  }
+
+};
+
+} // namespace kaleidoscope {
