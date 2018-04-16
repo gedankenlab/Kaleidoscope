d0a76ad10d6ca0eba150ad3d7082ebacb2b7ec20
Michael Richters <gedankenexperimenter@gmail.com>
2018-04-16 18:12:36 -0500

    Changed fifth modifier flag bit
    Now it's like Kaleidoscope again, with the first four bits being left mods, and the fifth
    being right alt.

--- src/kaleidoscope/Key/Keyboard.cpp
+++ src/kaleidoscope/Key/Keyboard.cpp
@@ -1,7 +1,6 @@
 // -*- c++ -*-
 
 #include "kaleidoscope/Key/Keyboard.h"
-#include "kaleidoscope/cKey.h"
 
 
 namespace kaleidoscope {
@@ -12,18 +11,60 @@ byte Key::Keyboard::modifiers() const {
   // to match the right-hand modifier bits in the HID report modifiers byte.
   if (mods_right_)
     modifiers <<= 4;
+  // If the keycode itself is also a modifier, include that as well
+  modifiers |= keycodeModifier();
+  // byte keycode_mod_bit = keycode_ - mod_keycode_offset;
+  // if (keycode_mod_bit < 8)
+  //   modifiers |= (1 << keycode_mod_bit);
   return modifiers;
 }
 
-// `cKey::first_modifier` should probably be `cKey::Keyboard::first_modifier`
-bool Key::Keyboard::isModifier() const {
-  return (keycode_ >= cKey::first_modifier.keycode_ &&
-          keycode_ <= cKey::last_modifier.keycode_     );
+inline
+byte Key::Keyboard::modifierFlags() const {
+  byte modifiers{mods_};
+  if (mods_right_)
+    modifiers <<= 4;
+  return modifiers;
 }
 
+inline
+bool Key::Keyboard::isTrueShift() const {
+  //return { isModifier() && (modifiers() & mod_shift_flags) };
+
+  // We only consider a key to be a "true" `shift` key if it encodes only modifiers,
+  // including either the left or right `shift` bits
+  if (byte modifiers = keycodeModifier()) {
+    // In this case, there's no need to shift the mod bits because we don't care if the
+    // `shift` modifier is left or right
+    modifiers |= mods_;
+    return { modifiers & mod_shift_flags };
+  } else {
+    return false;
+  }
+}
+
+// This method is rendered obsolete by the more useful keycodeModifier() method
+inline
+bool Key::Keyboard::isModifier() const {
+  return { keycodeModifier() };
+}
+
+// Return the modifiers byte for the keycode part of the Key object, if any
+inline
+byte Key::Keyboard::keycodeModifier() const {
+  byte modifiers{0};
+  byte keycode_mod_bit = keycode_ - mod_keycode_offset;
+  if (keycode_mod_bit < 8)
+    modifiers |= (1 << keycode_mod_bit);
+  return modifiers;
+}
+
+// This function doesn't really seem very useful. It's not helpful at compile time because
+// we have better, more efficient ways of setting the mod bits, and I'm struggling to come
+// up with any run-time use for it.
 void Key::Keyboard::addModifier(Key key) {
   assert(testType(key) && Key::Keyboard(key).isModifier());
-  byte mod_bit = Key::Keyboard(key).keycode_ - cKey::first_modifier.keycode_;
+  byte mod_bit = Key::Keyboard(key).keycode_ - mod_keycode_offset;
   if (mod_bit > 4) {
     mod_bit -= 4;
     mods_right_ = true;
--- src/kaleidoscope/Key/Keyboard.h
+++ src/kaleidoscope/Key/Keyboard.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "kaleidoscope/Key.h"
+#include "kaleidoscope/cKey/HIDTables.h"
 #include <assert.h>
 
 
@@ -11,7 +12,7 @@ namespace kaleidoscope {
 class Key::Keyboard {
 
  private:
-  byte keycode_ : 8, mods_ : 4, mods_right_ : 1, type_id_ : 3;
+  byte keycode_ : 8, mods_left_ : 4, mod_ralt_ : 1, type_id_ : 3;
 
   // These mod bits are in the same order as the mod bits in the HID keyboard report for
   // efficiency in passing them on. If they get rearranged, that will mess things up.
@@ -20,37 +21,54 @@ class Key::Keyboard {
   static constexpr byte mod_alt     { 0b0100 };  // (1 << 2)
   static constexpr byte mod_gui     { 0b1000 };  // (1 << 3)
 
+  // Now I'm really starting to think that it's not worth the trouble to have the
+  // mods_right_ member, and it would be simpler to just use the 5 mod flags from
+  // Kaleidoscope as-is.
+
+  // These belong in the report, not here? I'm not sure where to put these, but they
+  // should be useful for answering questions like "is this key a pure modifier key with
+  // shift set?"
+  static constexpr byte mod_control_flags { mod_control | mod_control << 4 };
+  static constexpr byte mod_shift_flags   { mod_shift   | mod_shift   << 4 };
+  static constexpr byte mod_alt_flags     { mod_alt     | mod_alt     << 4 };
+  static constexpr byte mod_gui_flags     { mod_gui     | mod_gui     << 4 };
+
+  static constexpr byte mod_keycode_offset { HID_KEYBOARD_LEFT_CONTROL };
+
  public:
   constexpr
   byte keycode() const { return keycode_; }
   byte modifiers() const;
+  byte keycodeModifier() const;
+  byte modifierFlags() const;
   bool isModifier() const;
   void addModifier(Key key);
-  void setModifiers(byte mods, bool mods_right = false);
-  void addModifiers(byte mods, bool mods_right = false);
+  void setModifiers(byte mods, bool mod_ralt = false);
+  void setModifiers(bool control, bool shift, bool alt, bool gui, bool ralt = false);
+  void addModifiers(byte mods, bool mod_ralt = false);
   void clearModifiers() {
     setModifiers(0);
   }
 
   // Run-time constructor; generates and empty Key (equal to cKey::blank)
   Keyboard() : keycode_    (0),
-               mods_       (0),
-               mods_right_ (false),
+               mods_left_  (0),
+               mod_ralt_   (false),
                type_id_    (Key::keyboard_type_id) {}
 
   // The primary constructor; sets all bit fields in the structure. This is what is used
   // to define Key::Keyboard compile-time constants.
   constexpr explicit
-  Keyboard(byte keycode, byte mods = 0, bool mods_right = false)
-      : keycode_(keycode), mods_(mods), mods_right_(mods_right),
+  Keyboard(byte keycode, byte mods_left = 0, bool mod_ralt = false)
+      : keycode_(keycode), mods_left_(mods_left), mod_ralt_(mod_ralt),
         type_id_(Key::keyboard_type_id) {}
 
   // Conversion from Key object. The caller should first verify that it's got the
   // correct type of Key object before calling this constructor.
   explicit
   Keyboard(Key key) : keycode_    (uint16_t(key)               ),
-                      mods_       (uint16_t(key) >>  8         ),
-                      mods_right_ (uint16_t(key) >> (8 + 4)    ),
+                      mods_left_  (uint16_t(key) >>  8         ),
+                      mod_ralt_   (uint16_t(key) >> (8 + 4)    ),
                       type_id_    (uint16_t(key) >> (8 + 4 + 1))  {
     assert(type_id_ == Key::keyboard_type_id);
   }
@@ -59,8 +77,8 @@ class Key::Keyboard {
   constexpr
   operator Key() const {
     return Key( keycode_                   |
-                mods_       <<  8          |
-                mods_right_ << (8 + 4)     |
+                mods_left_  <<  8          |
+                mod_ralt_   << (8 + 4)     |
                 type_id_    << (8 + 4 + 1)   );
   }
 
