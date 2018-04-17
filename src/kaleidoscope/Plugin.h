e5411cc57cdddee454a1c55b09caf0ed31b7317c
Michael Richters <gedankenexperimenter@gmail.com>
2018-04-17 10:13:06 -0500

    Removed unused plugin id code

--- src/kaleidoscope/Plugin.h
+++ src/kaleidoscope/Plugin.h
@@ -13,11 +13,6 @@ namespace kaleidoscope {
 class Plugin {
 
  public:
-  // Maybe this should be `getId()`, and take a Controller as the parameter?
-  virtual void init(byte id) final {
-    if (id_ == 0xFF)
-      id_ = id;
-  }
 
   // I might decide to break this up into multiple hooks
   virtual bool keyswitchEventHook(KeyswitchEvent& event, Plugin*& caller) {
@@ -33,28 +28,6 @@ class Plugin {
   virtual void preScanHook(uint16_t current_time) {}
   
  protected:
-
-  // Maybe I've got this all wrong, and the caller should be a Plugin* (or void*).
-  static constexpr byte controller_id_ {0xFF};
-
-  // Unique ID number for the plugin. This will also be the index number in the
-  // Controller's internal plugin array (but that shouldn't matter from here), which will
-  // allow the Controller to store values and later call the correct plugin based on those
-  // values. The default is all ones, which is an invalid value (if we ever have more than
-  // 255 plugins, we'll need a bigger integer type), reserved for the Controller's ID
-  // (when calling the keyswitch event hooks).
-  byte id_ {controller_id_};
-
-  // This function is used to prevent handling the same event twice; use it in event
-  // handler hook functions for all plugins -- if it returns true, it means that the
-  // plugin should ignore this event.
-  virtual bool checkCallerId(byte& caller_id) const final {
-    if (caller_id == controller_id_)
-      return false;
-    if (caller_id == this->id_)
-      caller_id = controller_id_;
-    return true;
-  }
 
   // This should maybe use a void* instead of a Plugin*. Also, maybe this should be in
   // keyswitchEventHook instead? (with the return value reversed). No, and NO!
