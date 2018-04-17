69ced63c996e0a65b6e212931789a01a7c56e593
Michael Richters <gedankenexperimenter@gmail.com>
2018-04-17 10:15:50 -0500

    Rearranged and commented hook methods

--- src/kaleidoscope/Plugin.h
+++ src/kaleidoscope/Plugin.h
@@ -14,18 +14,21 @@ class Plugin {
 
  public:
 
+  // Run every cycle before the keyswitch scan
+  virtual void preScanHook(uint16_t current_time) {}
+
   // I might decide to break this up into multiple hooks
   virtual bool keyswitchEventHook(KeyswitchEvent& event, Plugin*& caller) {
     return true;
   }
 
+  // Should probably be renamed preKeyboardReportHook()
   virtual bool preReportHook(hid::keyboard::Report& keyboard_report) {
     return true;
   }
 
+  // Should probably be renamed postKeyboardReportHook()
   virtual void postReportHook(KeyswitchEvent event) {}
-
-  virtual void preScanHook(uint16_t current_time) {}
   
  protected:
 
