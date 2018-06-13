6b5879c39431e3bdf47322b02cadb71aae42c1d7
Michael Richters <gedankenexperimenter@gmail.com>
2018-06-12 23:32:08 -0500

    Added `injected` bit to KeyState class

--- src/kaleidoglyph/KeyState.h
+++ src/kaleidoglyph/KeyState.h
@@ -9,6 +9,13 @@ namespace kaleidoglyph {
 
 class KeyState {
 
+ private:
+  byte raw_;
+
+  static constexpr byte curr_bit     {0b001};
+  static constexpr byte prev_bit     {0b010};
+  static constexpr byte injected_bit {0b100};
+
  public:
 
   constexpr
@@ -17,54 +24,40 @@ class KeyState {
   // Constructor sets all reserved bits to zero.
   explicit constexpr
   KeyState(byte state)
-    : raw_(state & 0x03) {}
+    : raw_(state & 0b111) {}
 
   constexpr
-  KeyState(bool curr, bool prev)
-    : raw_(curr | (prev << 1)) {}
+  KeyState(bool curr, bool prev, bool injected = false)
+    : raw_(curr | (prev << 1) | (injected << 2)) {}
 
   // These comparisons all assume that the reserved bits are all zero.
   bool isInactive() const {
-    return raw_ == 0; // 0b00 (previous = 0; current = 0;)
-  }
-
-  bool isPressed() const {
-    return raw_ == 1; // 0b01 (previous = 0; current = 1;)
-  }
-
-  bool isReleased() const {
-    return raw_ == 2; // 0b10 (previous = 1; current = 0;)
-  }
-
-  bool isHeld() const {
-    return raw_ == 3; // 0b11 (previous = 1; current = 1;)
+    return (raw_ & 0b011) == 0; // 0b00 (previous = 0; current = 0;)
   }
 
   bool toggledOn() const {
-    return isPressed();
+    return (raw_ & 0b001) == 1; // 0b01 (previous = 0; current = 1;)
   }
 
   bool toggledOff() const {
-    return isReleased();
+    return (raw_ & 0b011) == 2; // 0b10 (previous = 1; current = 0;)
   }
 
-  bool current() const {
-    return raw_ & 1;
+  bool isHeld() const {
+    return (raw_ & 0b011) == 3; // 0b11 (previous = 1; current = 1;)
   }
 
-  bool previous() const {
-    return raw_ & 2;
+  bool isInjected() const {
+    return raw_ & injected_bit;
   }
 
-  // This operator is probably unnecessary; the above functions should get us everything we need
-  bool operator!() const {
-    // return (! current && ! previous);
-    // return !(raw_ & B11);
-    return (! raw_);
+  bool isPressed() const {
+    return raw_ & curr_bit;
   }
 
- private:
-    byte raw_;
+  bool wasPressed() const {
+    return raw_ & prev_bit;
+  }
 
 };
 
