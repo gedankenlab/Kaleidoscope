77cca4f111ce00fd2a734102b11cc128f9ed6065
Michael Richters <gedankenexperimenter@gmail.com>
2019-02-18 19:46:27 -0600

    Comment regarding event id numbers
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/kaleidoglyph/KeyEvent.h
+++ src/kaleidoglyph/KeyEvent.h
@@ -10,6 +10,10 @@
 
 namespace kaleidoglyph {
 
+// I want to consider adding a serial number to KeyEvent, so that plugins can identify
+// events that they record in their `onKeyEvent` handlers, and act on that event showing
+// up in the `postKeyboardReport`, event if some of the other properites got changed along
+// the way.
 struct KeyEvent {
 
   Key       key;
