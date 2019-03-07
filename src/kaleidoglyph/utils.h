a25b706ffa5c70bb2338bd0ad35e77629b4df280
Michael Richters <gedankenexperimenter@gmail.com>
2019-03-07 10:23:18 -0600

    Make bitfieldSize() more general
    Using a template, it can now calculate bitfield sizes based on different
    units (e.g. `uint16_t`, in case the number of bits is large enough to warrant
    that). That's pretty unlikely on a keyboard, but still possible.
    
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- src/kaleidoglyph/utils.h
+++ src/kaleidoglyph/utils.h
@@ -9,9 +9,15 @@
 
 namespace kaleidoglyph {
 
-/// Calculates the number of bytes needed to store a bitfield of the given size in bits
-constexpr byte bitfieldSize(int bits) {
-  return ((bits - 1) / 8) + 1;
+// Return the number of `UnitType` units required to store `n` bits. Both `UnitType` &
+// `WidthType` should be integer types. `WidthType` is whatever type the parameter `n` is
+// stored as, and can be deduced bye the compiler, so it's not necessary to declare it
+// when calling this function (e.g. `bitfieldSize<uint16_t>(n)`). The default `UnitType`
+// is `byte` (i.e. `uint8_t`, which is almost always what we want, so most of the time we
+// can also drop that template parameter (e.g. `bitfieldSize(n)`).
+template <typename UnitType = byte, typename WidthType>
+constexpr WidthType bitfieldSize(WidthType n) {
+  return ((n - 1) / (8 * sizeof(UnitType))) + 1;
 }
 
 /// Calculates the number of bytes needed to store a bitfield of the given size in bits
